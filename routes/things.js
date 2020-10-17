1ee9c8d91e649bfb451c56d2fabcc961f89e0d71
Dillon <dillon@pop-os.localdomain>
2020-10-17 13:52:14 -0400

    Add things/id route

--- routes/things.js
+++ routes/things.js
@@ -3,14 +3,17 @@ const router = express.Router()
 
 const thingsData = [
     {
+        id: "A",
         name: "Impact driver",
         available: 2
     },
     {
+        id: "B",
         name: "Ladder",
         available: 1
     },
     {
+        id: "C",
         name: "Steam cleaner",
         available: 0
     }
@@ -28,6 +31,15 @@ router.get('/available', (req, res) => {
     const response = {
         status: "OK",
         things: thingsData.filter(t => t.available > 0)
+    }
+    res.send(response)
+})
+
+router.get('/:id', (req, res) => {
+    const thingId = req.params.id
+    const response = {
+        status: "OK",
+        things: thingsData.find(t => t.id == thingId)
     }
     res.send(response)
 })
