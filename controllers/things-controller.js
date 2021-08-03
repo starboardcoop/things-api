e5c5d34e1471de23806a66a2a0aae927df149e81
Dillon Fagan <dillonfagan@pm.me>
2021-08-03 06:48:51 -0400

    Add getThings func to get all things

--- controllers/things-controller.js
+++ controllers/things-controller.js
@@ -2,14 +2,41 @@ var Airtable = require('airtable')
 var base = new Airtable({ apiKey: 'key7xmscTskxd2T75' }).base('appBYEa4vGVLAXEbe')
 const table = base('Things')
 
-const getAll = async () => {
-    const result =  await table.select({
+const getThings = (req, res) => {
+    let records = []
+
+    // called for every page of records
+    const processPage = (partialRecords, fetchNextPage) => {
+        records = [...records, ...partialRecords]
+        fetchNextPage()
+    }
+
+    // called when all the records have been retrieved
+    const processRecords = (err) => {
+        if (err) {
+            console.error(err)
+            res.send({
+                status: "ERROR",
+                error: err
+            })
+            return
+        }
+
+        let things = records.map(minify)
+        res.send({
+            status: "OK",
+            things: things,
+            categories: getCategories(),
+            locations: getLocations()
+        })
+    }
+
+    table.select({
         view: 'All Things',
         fields: ["Name", "Category", "Stock", "Location", "Image"],
-        filterByFormula: "NOT({Hidden})"
-    }).firstPage()
-
-    return result.map(minify)
+        filterByFormula: "NOT({Hidden})",
+        pageSize: 100
+    }).eachPage(processPage, processRecords)
 }
 
 const minify = (thing) => {
@@ -54,7 +81,5 @@ const getLocations = () => {
 }
 
 module.exports = {
-    getAll,
-    getCategories,
-    getLocations
+    getThings
 }
--- routes/things.js
+++ routes/things.js
@@ -3,13 +3,7 @@ const router = express.Router()
 const ThingsController = require('../controllers/things-controller')
 
 router.get('/', async (req, res) => {
-    const response = {
-        status: "OK",
-        things: await ThingsController.getAll(),
-        categories: ThingsController.getCategories(),
-        locations: ThingsController.getLocations()
-    }
-    res.send(response)
+    ThingsController.getThings(req, res)
 })
 
 module.exports = router
