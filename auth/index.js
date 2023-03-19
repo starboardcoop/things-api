994077d20f66f01d53277662cd3c6544acfa9ab7
Dillon Fagan <dillonfagan@pm.me>
2023-03-19 18:11:47 -0400

    Add user access control by env whitelist

--- auth/index.js
+++ auth/index.js
@@ -10,7 +10,13 @@ const isAuthorized = (token) => {
     return Object.keys(tokens).includes(token);
 };
 
+const isWhitelisted = (email) => {
+    const whitelist = process.env.DISCORD_WHITELIST.split(' ');
+    return whitelist.includes(email);
+};
+
 module.exports = {
     authorize,
-    isAuthorized
+    isAuthorized,
+    isWhitelisted
 };
